87186fdd34f228de82faaae0d83db3d0deeeddc9
Danilo Righetto <danilonewtrue@gmail.com>
2021-03-22 21:40:40 -0300

    Componente com Propriedade

---
+++ fundamentos-do-react/src/components/basicos/ComParamentro.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function ComParametro(props) {
+
+  return <div>
+    <h2>Titulo: { props.titulo }</h2>
+    <h3>Subtitulo: { props.subtitulo }</h3>
+  </div>
+}
--- fundamentos-do-react/src/index.js
+++ fundamentos-do-react/src/index.js
@@ -2,6 +2,7 @@ import './index.css'
 import ReactDOM from 'react-dom';
 import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParamentro'
 
 const tag = <strong>Olá react - Danilo Righetto</strong>
 const el = document.getElementById('root');
@@ -9,6 +10,7 @@ ReactDOM.render(
   <div>
     <Primeiro></Primeiro>
     {tag}
+    <ComParametro titulo="Danilo" subtitulo="Righetto" />
   </div>,
   el
 );
