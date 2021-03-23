590439426289392ea68fdcb6737cdf74bbb3af15
Danilo Righetto <danilonewtrue@gmail.com>
2021-03-22 21:13:10 -0300

    Primeiro componente

---
+++ fundamentos-do-react/src/components/basicos/Primeiro.js
@@ -0,0 +1,5 @@
+function Primeiro() {
+  return 'Primeiro componente';
+}
+
+export default Primeiro;
--- fundamentos-do-react/src/index.js
+++ fundamentos-do-react/src/index.js
@@ -1,10 +1,13 @@
 import './index.css'
 import ReactDOM from 'react-dom';
 import React from 'react'
+import Primeiro from './components/basicos/Primeiro'
 
 const tag = <strong>Olá react - Danilo Righetto</strong>
 const el = document.getElementById('root');
 ReactDOM.render(
-  <div>{ tag }</div>,
+  <div>
+    <Primeiro></Primeiro>
+  </div>,
   el
 );
