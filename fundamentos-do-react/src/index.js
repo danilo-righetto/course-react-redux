4337542e3917fde2a9b7de4fd257018b1abe2431
Danilo Righetto <danilonewtrue@gmail.com>
2021-03-24 22:03:48 -0300

    React Fragment

--- fundamentos-do-react/src/index.js
+++ fundamentos-do-react/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParamentro'
+import Fragmento from './components/basicos/Fragmento'
 
 const tag = <strong>Olá react - Danilo Righetto</strong>
 const el = document.getElementById('root');
 ReactDOM.render(
-  <div>
+  <>
     <Primeiro></Primeiro>
     {tag}
     <ComParametro titulo="Danilo" subtitulo="Righetto" />
-  </div>,
+    <Fragmento />
+  </>,
   el
 );
