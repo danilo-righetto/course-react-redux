20f898c8273c2609a40e665c8d84e30596a73b18
Danilo Righetto <danilonewtrue@gmail.com>
2021-04-07 22:39:31 -0300

    Card #02

--- fundamentos-do-react/src/App.jsx
+++ fundamentos-do-react/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import './App.css';
 import Card from './components/layout/Card'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParamentro'
@@ -7,19 +8,21 @@ import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
 
 export default () => (
-  <div>
+  <div className="App">
     <h1>Fundamentos</h1>
-    <Card titulo="Desafio Aleatorio">
-      <Aleatorio min={10} max={20} />
-    </Card>
-    <Card titulo="Fragmento">
-      <Fragmento />
-    </Card>
-    <Card titulo="Com Parametro">
-      <ComParametro titulo="Danilo" subtitulo="Righetto" />
-    </Card>
-    <Card titulo="Primeiro">
-      <Primeiro></Primeiro>
-    </Card>
+      <div className="Cards">
+        <Card titulo="#04 - Desafio Aleatorio">
+          <Aleatorio min={10} max={20} />
+        </Card>
+        <Card titulo="#03 - Fragmento">
+          <Fragmento />
+        </Card>
+        <Card titulo="#02 - Com Parametro">
+          <ComParametro titulo="Danilo" subtitulo="Righetto" />
+        </Card>
+        <Card titulo="#01 - Primeiro">
+          <Primeiro></Primeiro>
+        </Card>
+      </div>
   </div>
 );
